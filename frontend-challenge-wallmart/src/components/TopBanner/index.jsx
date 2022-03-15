b85752c69d3f98e7ae2d78a16ffe80a3459ae774
Daniel Rivas <djra26111990@gmail.com>
2022-03-15 01:32:26 -0500

    Add TopBanner component

---
+++ frontend-challenge-wallmart/src/components/TopBanner/index.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+export const TopBanner = () => {
+  return (
+    <div className="flex flex-wrap items-center p-2 flex-col justify-center md:flex-row bg-venice-blue-600 py-2">
+      <span className="text-white text-xs mx-4 align-middle">Servicio al cliente de Lider.cl WhatsApp <strong className="text-venice-blue-200">+56957211492 </strong>  |  Horario: de Lunes a viernes de 8:00 a 22:00 y sábado, domingo y festivo de 9:00 a 22:00</span>
+    </div>
+  );
+};
