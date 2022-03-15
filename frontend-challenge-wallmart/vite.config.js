549f83b68f6b69f83720a342715a64d1679de7f7
Daniel Rivas <djra26111990@gmail.com>
2022-03-15 01:06:30 -0500

    Add packages to the project & configurations

--- frontend-challenge-wallmart/vite.config.js
+++ frontend-challenge-wallmart/vite.config.js
@@ -1,7 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import EnvironmentPlugin from 'vite-plugin-environment'
+require('dotenv').config()
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  define: {
+    "process.env": process.env
+  },
+  plugins: [react(), EnvironmentPlugin('all', { prefix: 'VITE_' })]
 })
